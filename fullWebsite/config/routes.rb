71f4509c1c1c5181479d3c07bc34c23d7df6f903
xDev <papesock1@gmail.com>
2022-11-15 17:37:06 +0100

    add template

--- fullWebsite/config/routes.rb
+++ fullWebsite/config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # create a root  the parameter to call the controller with the symbol # to call the action
-  get '/salut', to:'pages#salut'
+  # get '/salut', to:'pages#salut'
+  #if we want to give a parameter to the action :
+  #get '/salut/:name', to:'pages#salut'
+  # we can give a name for a Path  : by using as
+  get '/salut(/:name)', to:'pages#salut', as: 'salut'
+  root to:'pages#home' 
+  
 end
